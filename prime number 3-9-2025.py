d93b0e1af16b3007ff6e33eb17dea0924561e957
Krishna Chaitanya <chaitanyabezawada9@gmail.com>
2025-09-03 20:22:20 +0530

    Add files via upload

---
+++ prime number 3-9-2025.py
@@ -0,0 +1,10 @@
+n = int(input("num: "))
+i,temp=0,0
+for i in range(2,n//2):
+    if n%i == 0:
+        temp=1
+        break
+if temp == 1:
+    print("given number is not prime")
+else:
+    print("given number is prime")
