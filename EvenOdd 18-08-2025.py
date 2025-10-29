43969cb56bcc7716721b46fadd7c76faa96e7b58
Krishna Chaitanya <chaitanyabezawada9@gmail.com>
2025-10-29 21:10:47 +0530

    Delete EvenOdd 18-08-2025.py

--- EvenOdd 18-08-2025.py
+++
@@ -1,9 +0,0 @@
-num = int(input("Enter a Number:")) 
-if num % 2 == 0: 
-  print("Given number is Even") 
-else: 
-  print("Given number is Odd")
-
-#Output:
-#Enter a Number:121
-#Given number is Odd
