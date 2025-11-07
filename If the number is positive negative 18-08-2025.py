ccbbd9230280ef7daa11a670caa7579aa8301358
Krishna Chaitanya <chaitanyabezawada9@gmail.com>
2025-11-07 21:43:52 +0530

    Add files via upload

---
+++ If the number is positive negative 18-08-2025.py
@@ -0,0 +1,11 @@
+num = float(input("Enter a number: "))
+if num > 0:
+    print("The number is positive.")
+elif num == 0:
+    print("The number is zero.")
+else:
+    print("The number is negative.")
+
+#Output:
+#Enter a number: 24
+#The number is positive.
