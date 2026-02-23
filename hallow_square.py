851aedb08352ec9b517b0929671c8ecf375b71e9
Krishna Chaitanya <chaitanyabezawada9@gmail.com>
2026-02-23 12:52:48 +0530

    Add files via upload

---
+++ hallow_square.py
@@ -0,0 +1,18 @@
+def print_hollow_square(rows):
+    for i in range(rows):
+        if i == 0 or i == rows - 1:
+            print("*" * rows)
+        else:
+            print("*" + " " * (rows - 2) + "*")
+
+# Function call
+print_hollow_square(5)
+
+
+#output:
+
+#   *****
+#   *   *
+#   *   *
+#   *   *
+#   *****
