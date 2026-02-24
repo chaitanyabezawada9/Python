642c66e0e74a0022fae299675d7d89e8282b085a
Krishna Chaitanya <chaitanyabezawada9@gmail.com>
2026-02-24 14:21:26 +0530

    Add files via upload

---
+++ inverted_triangle.py
@@ -0,0 +1,16 @@
+def print_inverted_triangle(rows):
+    for i in range(rows, 0, -1):
+            print("*" * i)
+
+# Function call
+print_inverted_triangle(5)
+
+
+#output:
+
+#   **********
+#   ****
+#   ***
+#   **
+#   *
+
